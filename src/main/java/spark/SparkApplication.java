a9cbc51b698094bf7f2ded13df4d5df70f710b68
Suresh Babu K <85051490+sureshbabk19698@users.noreply.github.com>
2024-12-01 13:49:27 +0530

    Spark Basic setup

--- src/main/java/spark/SparkApplication.java
+++ src/main/java/spark/SparkApplication.java
@@ -1,13 +1,47 @@
 package spark;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+import java.util.List;
+
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@Slf4j
 public class SparkApplication {
+
+    @Value("${hadoop.home.dir}")
+    private String hadoopHomeDir;
+
+    @PostConstruct
+    public void setUp() {
+        System.setProperty("hadoop.home.dir", hadoopHomeDir);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SparkApplication.class, args);
+
+//        SparkConf sc = new SparkConf().setAppName("SparkBasics").setMaster("local[*]");
+//        JavaSparkContext jsc = new JavaSparkContext(sc);
+//        List<Integer> input = new ArrayList<>();
+//        JavaRDD<Integer> res = jsc.parallelize(input);
+//        jsc.close();
+
+        try (SparkSession ss = SparkSession.builder().master("local[*]")
+                .appName("SparkBasics")
+                .getOrCreate();
+             JavaSparkContext jss = new JavaSparkContext(ss.sparkContext())) {
+            JavaRDD<Integer> result = jss.parallelize(List.of(1, 2, 3, 4, 5));
+            log.info("Numbers count: {} ", result.count());
+            log.info("Number of partitions: {} ", result.getNumPartitions());
+        }
+
+
     }
 }
--- src/main/java/spark/config/SparkConfig.java
+++ src/main/java/spark/config/SparkConfig.java
@@ -1,8 +1,12 @@
 package spark.config;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class SparkConfig {
@@ -10,5 +14,8 @@ public class SparkConfig {
     public void getSparkConfig() {
         SparkConf sc = new SparkConf().setAppName("SparkBasics").setMaster("local[*]");
         JavaSparkContext jsc = new JavaSparkContext(sc);
+        List<Integer> input = new ArrayList<>();
+        JavaRDD<Integer> res = jsc.parallelize(input);
+        jsc.close();
     }
 }
