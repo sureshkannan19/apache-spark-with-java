f4b0a9149cf1360ab8e17dd992e21c0a63c18e25
Suresh Babu K <85051490+sureshbabk19698@users.noreply.github.com>
2024-12-01 21:13:01 +0530

    DataFrame

--- src/main/java/spark/SparkApplication.java
+++ src/main/java/spark/SparkApplication.java
@@ -4,11 +4,17 @@ import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructType;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -34,7 +40,21 @@ public class SparkApplication {
             log.info("Numbers count: {} ", result.count());
             log.info("Number of partitions: {} ", result.getNumPartitions());
 
-//            Dataset<Row> result = ss.sql("SELECT * FROM people WHERE age > 30");
+            StructType structType = new StructType();
+            structType = structType.add("Alphabet_Char", DataTypes.StringType, false);
+            structType = structType.add("Alphabet_Pos", DataTypes.StringType, false);
+
+            List<Row> nums = new ArrayList<>();
+            nums.add(RowFactory.create("A", "1"));
+            nums.add(RowFactory.create("B", "2"));
+            nums.add(RowFactory.create("C", "3"));
+
+
+            Dataset<Row> df = ss.createDataFrame(nums, structType);
+            df.createOrReplaceTempView("Alphabets");
+
+            Dataset<Row> temp = ss.sql("SELECT * FROM Alphabets WHERE Alphabet_Pos > 2");
+            temp.show();
             try (var scanner = new Scanner(System.in)) {
                 scanner.nextLine();
             }
