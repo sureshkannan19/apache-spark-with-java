92b4459a49f1fc08914fad3d89f266a72066bcef
Suresh Babu K <85051490+sureshbabk19698@users.noreply.github.com>
2024-12-03 19:18:39 +0530

    Updated README.md

--- src/main/java/spark/controller/SparkController.java
+++ src/main/java/spark/controller/SparkController.java
@@ -3,10 +3,7 @@ package spark.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.RowFactory;
-import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.*;
 import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.StructType;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,9 +39,7 @@ public class SparkController {
 
     @GetMapping("/dataFrame")
     public boolean dataFrame() {
-        StructType structType = new StructType();
-        structType = structType.add("Character", DataTypes.StringType, false);
-        structType = structType.add("Position", DataTypes.StringType, false);
+        StructType structType = new StructType().add("Character", DataTypes.StringType, false).add("Position", DataTypes.StringType, false);
 
         List<Row> nums = new ArrayList<>();
         nums.add(RowFactory.create("A", "1"));
@@ -57,6 +52,28 @@ public class SparkController {
 
         Dataset<Row> temp = sparkSession.sql("SELECT * FROM Alphabets WHERE Position > 2");
         temp.show();
+        return true;
+    }
+
+    @GetMapping("/dataFrameViaFile")
+    public boolean dataFrameViaFile() {
+        StructType structType = new StructType().add("Character", DataTypes.StringType, false).add("Position", DataTypes.StringType, false);
+
+//        JavaRDD<String> fakeData = sparkContext.textFile("fake_data.csv");
+        String path = getClass().getResource("/fake_data.csv").getPath();
+        Dataset<Row> df = sparkSession.read()
+                .option("header", "true")        // Use the first row as header
+                .option("inferSchema", "true")  // Automatically infer column data types
+                .csv(path);
+
+        df.createOrReplaceTempView("fake_data");
+        Dataset<Row> temp = sparkSession.sql("select country, count(*) as Youngsters_Count from fake_data where age between 20 and 50 group by country"); // Max youngsters country vise
+        temp.show(); // top 20 records
+
+        Dataset<Row> youngstersAcrossCountriesDf =  df.filter(new Column("age").between(20, 50));
+        log.info("Number of people between age 20 and 50 across the country {}",  youngstersAcrossCountriesDf.count()); // filter data by age
+        youngstersAcrossCountriesDf.groupBy("country").agg(functions.count("*").alias("Youngsters_Count")).show(); // Max youngsters country vise
+
         return true;
     }
 }
