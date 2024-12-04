655c4fcc1ccd76ddfa82842b6db55baf3cec1547
Suresh Babu K <85051490+sureshbabk19698@users.noreply.github.com>
2024-12-04 15:05:51 +0530

    Updated README.md

--- src/main/java/spark/SparkApplication.java
+++ src/main/java/spark/SparkApplication.java
@@ -1,10 +1,11 @@
 package spark;
 
-import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 @Slf4j
